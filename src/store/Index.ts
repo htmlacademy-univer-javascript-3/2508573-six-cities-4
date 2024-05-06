0b9ca7f9fd0814fcfa753b1e69c299f718ab08dc
MelkovaSona <Sofia.Melkova@urfu.me>
2024-05-06 22:24:08 +0500

    Register thunk middleware

--- src/store/Index.ts
+++ src/store/Index.ts
@@ -1,6 +1,15 @@
 ﻿import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './Reducer';
+import { createAPI } from '../services/Api';
+
+export const api = createAPI();
 
 export const store = configureStore({
   reducer: reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
 });
