f4168572d511e84842ec2b466b791552925d64dc
MelkovaSona <Sofia.Melkova@urfu.me>
2024-05-28 20:18:40 +0500

    Add tests for extrareducers in CurrentOfferSlice

--- src/store/currentOffer/CurrentOfferSlice.test.ts
+++ src/store/currentOffer/CurrentOfferSlice.test.ts
@@ -1,6 +1,16 @@
-﻿import { currentOfferSlice, fillReviews, addReview, fillNearbyOffers, updateOffer, clearOffer, CurrentOfferState } from './CurrentOfferSlice';
+﻿import {
+  currentOfferSlice,
+  fillReviews,
+  addReview,
+  fillNearbyOffers,
+  updateOffer,
+  clearOffer,
+  CurrentOfferState,
+} from './CurrentOfferSlice';
 import { generateOffer } from '../../mocks/Offer';
 import { generateReview } from '../../mocks/Review';
+import { changeFavoriteStatus } from '../offers/OffersSlice';
+import { datatype } from 'faker';
 
 describe('Current Offer slice', () => {
   let initialState: CurrentOfferState;
@@ -24,33 +34,45 @@ describe('Current Offer slice', () => {
   it('should fill reviews with \'fillReviews\' action', () => {
     const reviews = [generateReview()];
 
-    const result = currentOfferSlice.reducer(initialState, fillReviews(reviews));
+    const result = currentOfferSlice.reducer(
+      initialState,
+      fillReviews(reviews)
+    );
 
     expect(result.reviews).toBe(reviews);
   });
 
   it('should add review with \'addReview\' action', () => {
-    const newReview = generateReview();
+    const review = generateReview();
 
-    const result = currentOfferSlice.reducer(initialState, addReview(newReview));
+    const result = currentOfferSlice.reducer(
+      initialState,
+      addReview(review)
+    );
 
-    expect(result.reviews).toContainEqual(newReview);
+    expect(result.reviews).toContainEqual(review);
   });
 
   it('should fill nearby offers with \'fillNearbyOffers\' action', () => {
     const nearbyOffers = [generateOffer(), generateOffer(), generateOffer()];
 
-    const result = currentOfferSlice.reducer(initialState, fillNearbyOffers(nearbyOffers));
+    const result = currentOfferSlice.reducer(
+      initialState,
+      fillNearbyOffers(nearbyOffers)
+    );
 
     expect(result.nearbyOffers).toEqual(nearbyOffers);
   });
 
   it('should update current offer with \'updateOffer\' action', () => {
-    const newOffer = generateOffer();
+    const offer = generateOffer();
 
-    const result = currentOfferSlice.reducer(initialState, updateOffer(newOffer));
+    const result = currentOfferSlice.reducer(
+      initialState,
+      updateOffer(offer)
+    );
 
-    expect(result.offer).toEqual(newOffer);
+    expect(result.offer).toEqual(offer);
   });
 
   it('should clear current offer with \'clearOffer\' action', () => {
@@ -66,5 +88,28 @@ describe('Current Offer slice', () => {
     expect(result.reviews).toHaveLength(0);
     expect(result.nearbyOffers).toHaveLength(0);
   });
+
+  it('should update favorite status of current offer when "changeFavoriteStatus" action is dispatched', () => {
+    const offer = generateOffer();
+    initialState.offer = offer;
+
+    const result = currentOfferSlice.reducer(
+      initialState,
+      changeFavoriteStatus({ offerId: offer.id, isFavorite: true })
+    );
+
+    expect(result.offer?.isFavorite).toEqual(true);
+  });
+
+  it('should not update favorite status if offerId does not match current offer', () => {
+    const offer = generateOffer();
+    initialState.offer = offer;
+
+    const result = currentOfferSlice.reducer(
+      initialState,
+      changeFavoriteStatus({ offerId: datatype.uuid(), isFavorite: true })
+    );
+
+    expect(result.offer?.isFavorite).toEqual(false);
+  });
 });
-// TODO: добавить тест на extrareducers
