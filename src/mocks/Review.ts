b02e635d0a4fab37db504319bc3c9a9d53641c56
MelkovaSona <Sofia.Melkova@urfu.me>
2024-05-28 19:16:46 +0500

    Add mocks

---
+++ src/mocks/Review.ts
@@ -0,0 +1,11 @@
+﻿import faker from 'faker';
+import { Review } from '../entities/Review';
+import { UserData } from '../entities/User';
+
+export const generateReview = (userData: UserData): Review => ({
+  id: faker.datatype.uuid(),
+  date: faker.date.past().toISOString(),
+  user: userData,
+  comment: faker.lorem.paragraph(),
+  rating: faker.datatype.number({ min: 1, max: 5 }),
+});
---
+++ src/mocks/User.ts
@@ -0,0 +1,10 @@
+﻿import faker from 'faker';
+import { User } from '../entities/User';
+
+export const generateUser = (): User => ({
+  email: faker.internet.email(),
+  token: faker.random.alphaNumeric(16),
+  name: faker.name.findName(),
+  avatarUrl: faker.image.avatar(),
+  isPro: faker.datatype.boolean(),
+});
