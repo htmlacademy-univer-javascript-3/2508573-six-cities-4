a59ceae2e6b9d603980bb0aa0e6128af729368e5
MelkovaSona <Sofia.Melkova@urfu.me>
2024-05-29 17:31:54 +0500

    Add test for ReviewItem

---
+++ src/pages/offer/reviews/ReviewItem.test.tsx
@@ -0,0 +1,33 @@
+﻿import { render, screen } from '@testing-library/react';
+import { Review } from '../../../entities/Review';
+import ReviewItem from './ReviewItem';
+import { generateUser } from '../../../mocks/User';
+
+describe('Component: ReviewItem', () => {
+  const mockReview: Review = {
+    date: '2022-01-15',
+    user: generateUser(),
+    comment: 'Great experience!',
+    rating: 4,
+    id: '1'
+  };
+
+  const reviewsTextId = 'reviews__text';
+  const reviewsTimeId = 'reviews__time';
+  const reviewsInfoContainerId = 'reviews__info-container';
+
+  it('should render correct review information', () => {
+    const expectedDateText = 'January 2022';
+    const expectedDateTime = '2022-01-15';
+
+    render(<ReviewItem {...mockReview} />);
+    const reviewsInfoContainer = screen.getByTestId(reviewsInfoContainerId);
+    const reviewText = screen.getByTestId(reviewsTextId);
+    const reviewTime = screen.getByTestId(reviewsTimeId);
+
+    expect(reviewsInfoContainer).toBeInTheDocument();
+    expect(reviewText).toHaveTextContent(mockReview.comment);
+    expect(reviewTime).toHaveTextContent(expectedDateText);
+    expect(reviewTime).toHaveAttribute('datetime', expectedDateTime);
+  });
+});
--- src/pages/offer/reviews/ReviewList.tsx
+++ src/pages/offer/reviews/ReviewList.tsx
@@ -4,17 +4,17 @@ import ReviewItem from './ReviewItem';
 export default function ReviewList({ reviews }: { reviews: Review[] }) {
   return (
     <>
-      <h2 className="reviews__title">
+      <h2 className="reviews__title" data-testid="reviews_title">
         Reviews · <span className="reviews__amount">{reviews.length}</span>
       </h2>
-      <ul className="reviews__list">
+      <ul className="reviews__list" data-testid="reviews__items-container">
         {reviews
           .toSorted(
             (x, y) => new Date(y.date).getTime() - new Date(x.date).getTime()
           )
           .slice(0, 10)
           .map((review) => (
-            <li key={review.id} className="reviews__item">
+            <li key={review.id} className="reviews__item" data-testid="reviews__item">
               <ReviewItem {...review} />
             </li>
           ))}
