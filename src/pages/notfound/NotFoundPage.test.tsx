fc170fdeb49b54bc2ed83638b28c17fc6e3e8d8f
MelkovaSona <Sofia.Melkova@urfu.me>
2024-05-29 18:51:27 +0500

    Add test for NotFoundPage

---
+++ src/pages/notfound/NotFoundPage.test.tsx
@@ -0,0 +1,17 @@
+﻿import { render, screen } from '@testing-library/react';
+import NotFoundPage from './NotFoundPage';
+import { withHistory } from '../../mocks/MockComponent';
+
+describe('Component: NotFoundPage', () => {
+  it('should render correctly', () => {
+    render(withHistory(<NotFoundPage />));
+
+    const notFoundText = screen.getByText('Page not found');
+    const goToHomePageButton = screen.getByRole('link', {
+      name: 'Go to home page',
+    });
+
+    expect(notFoundText).toBeInTheDocument();
+    expect(goToHomePageButton).toBeInTheDocument();
+  });
+});
