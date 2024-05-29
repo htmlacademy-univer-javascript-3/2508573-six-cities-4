7b2a7dbbc495b8426a0eeb31e720aed687dd75a1
MelkovaSona <Sofia.Melkova@urfu.me>
2024-05-29 18:23:22 +0500

    Add test for LoactionHeader

--- src/components/locations/LocationHeader.tsx
+++ src/components/locations/LocationHeader.tsx
@@ -11,9 +11,9 @@ export default function LocationHeader({
   selected,
 }: LocationHeaderProps) {
   return (
-    <div className={cn('favorites__locations locations', {'locations--current': selected})}>
+    <div className={cn('favorites__locations locations', {'locations--current': selected})} data-testid="locations-container">
       <div className="locations__item">
-        <Link className="locations__item-link" to="/">
+        <Link className="locations__item-link" to="/" data-testid="locations__item-link">
           <span>{title}</span>
         </Link>
       </div>
---
+++ src/mocks/MockComponent.tsx
@@ -0,0 +1,12 @@
+﻿import { HelmetProvider } from 'react-helmet-async';
+import { MemoryRouter } from 'react-router-dom';
+
+export function withHistory(component: JSX.Element) {
+  return (
+    <MemoryRouter>
+      <HelmetProvider>
+        {component}
+      </HelmetProvider>
+    </MemoryRouter>
+  );
+};
