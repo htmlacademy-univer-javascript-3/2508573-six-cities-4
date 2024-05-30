a42422b5f01e1095ca61f60e7a5de2a972d8abd2
MelkovaSona <Sofia.Melkova@urfu.me>
2024-05-30 22:20:29 +0500

    Add tests for Map

---
+++ src/components/map/Map.test.tsx
@@ -0,0 +1,37 @@
+﻿import { render, screen } from '@testing-library/react';
+import Map from './Map';
+import { generateLocation } from '../../mocks/Location';
+
+describe('Component: Map', () => {
+  const mapContainerId = 'map';
+
+  it('should render correctly with given city and points', () => {
+    const mockCity = generateLocation();
+    const mockPoints = Array.from({ length: 3 }, generateLocation);
+
+    render(<Map city={mockCity} points={mockPoints} />);
+
+    const mapContainer = screen.getByTestId(mapContainerId);
+    expect(mapContainer).toBeInTheDocument();
+  });
+
+  it('should render correctly with selected location', () => {
+    const mockCity = generateLocation();
+    const mockPoints = Array.from({ length: 3 }, generateLocation);
+    const mockSelected = generateLocation();
+
+    render(<Map city={mockCity} points={mockPoints} selected={mockSelected} />);
+
+    const mapContainer = screen.getByTestId(mapContainerId);
+    expect(mapContainer).toBeInTheDocument();
+  });
+
+  it('should render correctly without points', () => {
+    const mockCity = generateLocation();
+
+    render(<Map city={mockCity} points={[]} />);
+
+    const mapContainer = screen.getByTestId(mapContainerId);
+    expect(mapContainer).toBeInTheDocument();
+  });
+});
